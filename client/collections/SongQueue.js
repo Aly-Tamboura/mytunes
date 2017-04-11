ebc6708bacd6022aeeff9f7f4ff6c47efa956a55
Hack Reactor Student <student@hackreactor.com>
2017-04-11 16:50:03 -0700

    Pass all the tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,31 +4,51 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-    this.on('add', function() {
-      if( this.models.length === 1 ) {
-      	this.playFirst()
-      }
-    });
-    this.on('ended', function() {
-    	this.shift();
-    	if( this.length) {
-	      this.playFirst();
-    	}
-    });
-    this.on('dequeue', function() {
-    	this.remove()
-    });
+    // this.on('add', function() {
+    //   if( this.models.length === 1 ) {
+    //   	this.playFirst()
+    //   }
+    // }, this);
+    // this.on('ended', function() {
+    // 	this.shift();
+    // 	if( this.length) {
+	   //    this.playFirst();
+    // 	}
+    // }, this);
+    // this.on('dequeue', function() {
+    // 	this.remove(song);
+    // }, this);
+    this.on('add', this.enqueue, this);
+    this.on('dequeue', this.dequeue, this);
+    this.on('ended', this.next, this);
   },
 
+  enqueue: function(song) {
+    if( this.models.length === 1 ) {
+      this.playFirst()
+    } else {
+      return;
+    }
+  },
+
+  dequeue: function(song) {
+    if (this.first() === song) {
+      this.next();
+    } else {
+      this.remove(song);
+    }
+  },
+
+  next: function() {
+    this.shift();
+    if (this.length) {
+      this.playFirst();
+    } else {
+      this.trigger('stop');
+    }
+  },
+  
   playFirst: function() {
-  	this.on('play', function() {
-      
-  	});
-
-  },
-
-  //add: function() {
-
-  //}
-
+    this.first().play();
+  }
 });
